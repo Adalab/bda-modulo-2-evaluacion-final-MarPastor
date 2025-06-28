ebb85759f923ec34d176dd28432dec8509fdc2b1
MarPastor <arq.marpastor@gmail.com>
2025-06-28 21:10:03 +0100

    Resolucion ejercicio 7. Enmendar alias en los ejercicios anteriores

--- final_evaluation_mod2_marpastor.sql
+++ final_evaluation_mod2_marpastor.sql
@@ -8,31 +8,34 @@ USE Sakila;
 -- 1. Selecciona todos los nombres de las películas sin que aparezcan duplicados
 
 SELECT DISTINCT f.title -- selecciona la columna con el nombre de las peliculas una vez con 'distinct'
-FROM film AS f; -- de la tabla 'film'
+FROM film AS f; -- de la tabla 'film' con un alias 'f'
 
 -- 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13"
 
 SELECT f.title, f.rating -- seleccina las columnas con el nombre y la clasificación de las películas
-FROM film AS f -- llama la tabla que contiene las columnas
+FROM film AS f -- llama la tabla que contiene las columnas con un alias 'f'
 WHERE rating = 'PG-13'; -- filtra la información requerida en el ejercicio
 
 -- 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción
 
 SELECT f.title, f.description -- selecciona las columnas de nombre y descripción
-FROM film AS f -- de la tabla 'film' donde estan las columnas
+FROM film AS f -- de la tabla 'film' con un alias 'f'
 WHERE f.description LIKE '%amazing%'; -- filtra las descripciones que contiene la palabra 'amazing' y lo que rodee la palabra
 
 -- 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos
 
 SELECT f.title, f.length -- seleccion el titulo y la duracion para comprobar resultados
-FROM film AS f -- de la tabla film
+FROM film AS f -- de la tabla film con un alias 'f'
 WHERE f.length > 120 -- filtra las peliculas que tengan una duracion mayor de 120
 ORDER BY f.length;
 
 -- 5. Recupera los nombres de todos los actores
 
 SELECT a.first_name, a.last_name -- selecciona las columnas del nombre y apellido de los actores
-FROM actor AS a; -- en la tabla 'actor'
+FROM actor AS a; -- en la tabla 'actor' con alias 'a'
+
+SELECT a.first_name -- selecciona la columna del nombre de los actores
+FROM actor AS a; -- en la tabla 'actor' con un alias 'a'
 
 SELECT CONCAT(first_name, ' ', last_name) AS nombre_actores -- selecciona los datos de las columnas nombre y apellidos, y las concatena en una sola columna
 FROM actor;
@@ -40,5 +43,12 @@ FROM actor;
 -- 6. Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido
 
 SELECT a.first_name, a.last_name -- selecciona las columnas de nombre y apellidos
-FROM actor AS a -- de la tabla 'actor'
+FROM actor AS a -- de la tabla 'actor' con un alias 'a'
 WHERE a.last_name LIKE '%Gibson%'; -- filtra la consulta con nombre y apellido de actores que contengan 'Gibson' en la columna 'last_name'
+
+-- 7. Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20
+
+SELECT a.first_name, a.actor_id -- selecciona la columna del nombre de los actores y el id de los actores para comprobar resultados
+FROM actor AS a -- de la tabla 'actor' con alias 'a'
+WHERE a.actor_id BETWEEN 10 AND 20 -- filtra los id de los actores entre 10 y 20, incluyendolos
+ORDER BY a.actor_id; -- ordena de mayor a menor por id
