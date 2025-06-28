94e260bd8ea19c43f6c964d7ac361853bee7327c
MarPastor <arq.marpastor@gmail.com>
2025-06-28 18:29:46 +0100

    Resolucion ejercicio 2

--- final_evaluation_mod2_marpastor.sql
+++ final_evaluation_mod2_marpastor.sql
@@ -7,8 +7,11 @@ USE Sakila;
 
 --  1. Selecciona todos los nombres de las películas sin que aparezcan duplicados
 
-SELECT DISTINCT title
-FROM film;
+SELECT DISTINCT title -- selecciona la columna con el nombre de las peliculas una vez con 'distinct'
+FROM film; -- de la tabla 'film'
 
 -- 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13"
 
+SELECT f.title, f.rating -- seleccina las columnas con el nombre y la clasificación de las películas
+FROM film AS f -- llama la tabla que contiene las columnas
+WHERE rating = 'PG-13'; -- filtra la información requerida en el ejercicio
